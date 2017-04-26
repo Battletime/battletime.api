1559badba1a767b2fa33bda5b3b19e0146655eb8
Stino <Linksonder@gmail.com>
2017-04-26 21:08:39 +0200

    stomme enviroment dingen

--- app/config.service.js
+++ app/config.service.js
@@ -2,8 +2,8 @@ angular.module('battletime-portal')
 .service('config', function(){
 
     return {
-        //apiRoot: "https://battletime.herokuapp.com/api",
-        apiRoot: "http://localhost:3000/api"
+        apiRoot: "https://battletime.herokuapp.com/api",
+        //apiRoot: "http://localhost:3000/api"
     }
 
 });
--- public/js/app.js
+++ public/js/app.js
@@ -120,8 +120,8 @@ angular.module('battletime-portal')
 .service('config', function(){
 
     return {
-        //apiRoot: "https://battletime.herokuapp.com/api",
-        apiRoot: "http://localhost:3000/api"
+        apiRoot: "https://battletime.herokuapp.com/api",
+        //apiRoot: "http://localhost:3000/api"
     }
 
 });
