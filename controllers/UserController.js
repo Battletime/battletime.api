923604e4444c5a485f14b9a361d9d88c83e24d1e
Stino <Linksonder@gmail.com>
2017-05-03 22:27:47 +0200

    debug logs

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -21,14 +21,30 @@ module.exports = function(){
     }
 
     self.uploadAvatar = function(userId, rawImage){
+        console.log("starting upload of avatar 2!");
         return new Promise(function (resolve, reject) {
             User.findById(userId).exec( (err, user) => {   
+
+                if(err){
+                     console.log("User not found with id " + userId);
+                    reject(err);
+                }
+                    
+                
+                console.log("User found with id " + user._id);
+
                 var base64Data = rawImage.replace(/^data:image\/png;base64,/, "");
                 var location = "/images/" + user._id + ".jpg";
                 require("fs").writeFile(location, base64Data, 'base64', function(err) {
-                    if(er)
-                        return reject(err);
+                    console.log("Write file complete");
 
+                    if(err){
+                        console.log("err wile writing file");
+                        console.log(err);
+                        return reject(err);
+                    }
+                        
+                    console.log("Sving user with new image location" + location);
                     user.imageUri = location;
                     user.save((err, user) =>  resolve(user)); 
                 });   
