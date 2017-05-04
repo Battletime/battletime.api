2cf8bbb6580b93b9aa1772d5db9d1dfa38571e68
Stino <Linksonder@gmail.com>
2017-05-04 18:37:24 +0200

    gulp vergetenn xD

--- public/js/app.js
+++ public/js/app.js
@@ -126,8 +126,8 @@ angular.module('battletime-portal')
 .service('config', function(){
 
     return {
-        //apiRoot: "https://battletime.herokuapp.com/api",
-        apiRoot: "http://localhost:3000/api"
+        apiRoot: "https://battletime.herokuapp.com/api", 
+        //apiRoot: "http://localhost:3000/api"
     }
 
 });
