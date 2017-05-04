cf3c7a7f53505a8adb1200930ac19ba3b827d45b
Stino <Linksonder@gmail.com>
2017-05-04 18:35:47 +0200

    hmmm

--- app/config.service.js
+++ app/config.service.js
@@ -2,7 +2,7 @@ angular.module('battletime-portal')
 .service('config', function(){
 
     return {
-        apiRoot: "https://battletime.herokuapp.com/api",
+        apiRoot: "https://battletime.herokuapp.com/api", 
         //apiRoot: "http://localhost:3000/api"
     }
 
