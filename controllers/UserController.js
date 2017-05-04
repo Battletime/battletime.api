311813435367936657dc84a60951bf479151d7c8
Stino <Linksonder@gmail.com>
2017-05-04 09:36:12 +0200

    upload met imgur

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 var User = mongoose.model("User");
 var _ = require('underscore');
-var multer  =   require('multer');
+var imgur = require('../services/imgur'),
+path = require('path');
 
+imgur.setClientID("d5147569a055369");
 
 module.exports = function(){
 
@@ -25,15 +27,22 @@ module.exports = function(){
             User.findById(userId).exec( (err, user) => {   
                 if(err || !rawImage)
                     return reject(err);
-                                    
+         
                 var base64Data = rawImage.replace(/^data:image\/jpeg;base64,/, "");
                 var location = "/images/" + user._id + ".jpg";
+   
                 require("fs").writeFile("public" + location, base64Data, 'base64', function(err) {
                     if(err)
                         return reject(err);
+
+
+                    //upload
+                    imgur.upload(path.join("public" + location), function (err, res) {
+                        user.imageUri = res.data.link;
+                        user.save((err, user) =>  resolve(user)); 
+                    });
                     
-                    user.imageUri = location;
-                    user.save((err, user) =>  resolve(user)); 
+                   
                 });   
             });     
         });
